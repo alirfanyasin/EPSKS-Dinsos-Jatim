84710b6adc5acf16b69fc2d6ae4a55cb330fbc0c
alirfanyasin <risayunita21@gmail.com>
2024-05-16 09:34:29 +0700

    reafctor: hidden button import in tksk pillar

--- resources/views/app/pillars/tksk/index.blade.php
+++ resources/views/app/pillars/tksk/index.blade.php
@@ -15,71 +15,76 @@
       </div>
     </div>
     <div class="section-body">
-        <h2 class="section-title">Data TKSK</h2>
-        <p class="section-lead">Tenga Kerja Sosial Kecamatan</p>
-        <div class="card">
-            <div class="card-header">
-                <h4>Data TKSK</h4>
-                <a class="btn btn-success" href="{{ route('app.pillar.tksk.create') }}" type="button">
-                    <i class="fas fa-plus"></i> Tambah TKSK
-                </a>
-                <a class="btn btn-primary ml-2" href="{{ route('app.pillar.tksk.import') }}" type="button">
+      <h2 class="section-title">Data TKSK</h2>
+      <p class="section-lead">Tenga Kerja Sosial Kecamatan</p>
+      <div class="card">
+        <div class="card-header">
+          <h4>Data TKSK</h4>
+          <a class="btn btn-success" href="{{ route('app.pillar.tksk.create') }}" type="button">
+            <i class="fas fa-plus"></i> Tambah TKSK
+          </a>
+          {{-- <a class="ml-2 btn btn-primary" href="{{ route('app.pillar.tksk.import') }}" type="button">
                     <i class="fas fa-file-import"></i> Import Data
-                </a>
-            </div>
-            <div class="card-body p-0 m-4">
-                <div class="table-responsive">
-                    <table class="table table-striped table-md" id="table-data">
-                        <thead>
-                            <tr>
-                                <th>No.</th>
-                                <th>No. Induk Anggota TKSK (NIAT)</th>
-                                <th>Lokasi Tugas</th>
-                                <th>Nama TKSK</th>
-                                <th>Nama Ibu Kandung</th>
-                                <th>No. KTP / NIK</th>
-                                <th>No. HP / Whatapp</th>
-                                <th>Tahun Pengangkatan TKSK</th>
-                                <th>Detail</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($tksks as $key => $tksk)
-                                <tr>
-                                    <td>{{ $key + 1 }}</td>
-                                    <td>{{ $tksk->membership_number }}</td>
-                                    <td>{{ $tksk->duty_address ? $tksk->duty_address['regency'] : '' }} , {{ $tksk->duty_address ? $tksk->duty_address['district'] : '' }}</td>
-                                    <td>{{ $tksk->name }}</td>
-                                    <td>{{ $tksk->mother_name }}</td>
-                                    <td>{{ $tksk->nik }}</td>
-                                    <td>{{ $tksk->phone_number }}</td>
-                                    <td>{{ $tksk->year_of_appointment }}</td>
-                                    <td @if(auth()->user()->hasRole('super-admin')) @endif>
-                                        <div class="d-flex flex-row flex-wrap">
-                                            <a href="{{ route('app.pillar.tksk.show', $tksk->hash) }}" class="btn btn-sm btn-info w-100" title="Detail">Detail Data</a>
-                                            <a href="{{ route('app.pillar.tksk.personal.report', $tksk->hash) }}" class="btn btn-sm btn-warning w-100 mt-2" title="Detail Laporan">Detail Laporan</a>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="d-flex flex-row flex-wrap">
-                                            <a href="{{ route('app.pillar.tksk.edit', $tksk->hash) }}" class="btn btn-sm btn-primary w-100" title="Edit">Edit Data</a>
-                                            <div class="w-100">
-                                                <form action="{{ route('app.pillar.tksk.delete', $tksk->hash) }}" class="formDelete" method="POST">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="submit" class="btn btn-sm btn-danger w-100 mt-2">Delete Data</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                </a> --}}
         </div>
+        <div class="p-0 m-4 card-body">
+          <div class="table-responsive">
+            <table class="table table-striped table-md" id="table-data">
+              <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>No. Induk Anggota TKSK (NIAT)</th>
+                  <th>Lokasi Tugas</th>
+                  <th>Nama TKSK</th>
+                  <th>Nama Ibu Kandung</th>
+                  <th>No. KTP / NIK</th>
+                  <th>No. HP / Whatapp</th>
+                  <th>Tahun Pengangkatan TKSK</th>
+                  <th>Detail</th>
+                  <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($tksks as $key => $tksk)
+                  <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $tksk->membership_number }}</td>
+                    <td>{{ $tksk->duty_address ? $tksk->duty_address['regency'] : '' }} ,
+                      {{ $tksk->duty_address ? $tksk->duty_address['district'] : '' }}</td>
+                    <td>{{ $tksk->name }}</td>
+                    <td>{{ $tksk->mother_name }}</td>
+                    <td>{{ $tksk->nik }}</td>
+                    <td>{{ $tksk->phone_number }}</td>
+                    <td>{{ $tksk->year_of_appointment }}</td>
+                    <td @if (auth()->user()->hasRole('super-admin'))  @endif>
+                      <div class="flex-row flex-wrap d-flex">
+                        <a href="{{ route('app.pillar.tksk.show', $tksk->hash) }}" class="btn btn-sm btn-info w-100"
+                          title="Detail">Detail Data</a>
+                        <a href="{{ route('app.pillar.tksk.personal.report', $tksk->hash) }}"
+                          class="mt-2 btn btn-sm btn-warning w-100" title="Detail Laporan">Detail Laporan</a>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="flex-row flex-wrap d-flex">
+                        <a href="{{ route('app.pillar.tksk.edit', $tksk->hash) }}" class="btn btn-sm btn-primary w-100"
+                          title="Edit">Edit Data</a>
+                        <div class="w-100">
+                          <form action="{{ route('app.pillar.tksk.delete', $tksk->hash) }}" class="formDelete"
+                            method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="mt-2 btn btn-sm btn-danger w-100">Delete Data</button>
+                          </form>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 @endsection
